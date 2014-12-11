ac59efca97fd9a23d2322b6c665bdf780e9d91e3
ipjohnson <ipjohnson123@yahoo.com>
2014-12-11 06:48:17 -0700

    Updating comments

--- src/SeleniumFixture/Using.cs
+++ src/SeleniumFixture/Using.cs
@@ -8,12 +8,12 @@ using SeleniumFixture.Impl;
 namespace SeleniumFixture
 {
     /// <summary>
-    /// PRovides custom implemenation of Selenium By class
+    /// Provides custom implemenation of Selenium By class
     /// </summary>
     public static class Using
     {
         /// <summary>
-        /// Decide which type of selector (jQuery, CSS, XPath) to use based on the type of selector and if jQuery is enabled for the site
+        /// Automatically decide which type of selector (jQuery, CSS, XPath) to use based on the type of selector and if jQuery is enabled for the site
         /// </summary>
         /// <param name="selector">selector string</param>
         /// <returns>By implementation</returns>
