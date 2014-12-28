18e1afdfeb183ccf3752299e763e1f276de811a0
ipjohnson <ipjohnson123@yahoo.com>
2014-12-28 12:09:33 -0700

    Adding nuspec files

---
+++ src/SeleniumFixture.xUnit/InternetExplorerDriverAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace SeleniumFixture.xUnit
+{
+    [DataDiscoverer("SeleniumFixture.xUnit.Impl.InternetExplorerDriverDataDiscoverer", "SeleniumFixture.xUnit")]
+    public class InternetExplorerDriverAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield break;
+        }
+    }
+}
